92e5cc6ff704d4c8d64c3edf9ab7c1f8ce57bab8
vsguerrier <vsguerrier@dinasa.ht>
2022-08-31 11:40:03 -0400

    login done : cors policy

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,22 +7,22 @@ export const Header = () => {
     <div className="px-3 py-2 bg-dark text-white">
       <div className="container">
         <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-          <Link href="/" className="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
+          <a href="/" className="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
             {/* <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg> */}
-          </Link>
+          </a>
 
           <ul className="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
             <li>
-              <Link to="/" className="nav-link text-secondary">
+              <Link to="/home" className="nav-link text-secondary">
                 {/* <svg className="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#home"></use></svg> */}
                 Home
               </Link>
             </li>
             <li>
-              <a href="#" className="nav-link text-white">
+              <Link to="/home" className="nav-link text-white">
                 {/* <svg className="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#speedometer2"></use></svg> */}
                 Dashboard
-              </a>
+              </Link>
             </li>
             <li>
               <a href="#" className="nav-link text-white">
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,18 +1,56 @@
-import React, { Component } from 'react';
+import axios from 'axios';
+import React, { Component,useState } from 'react';
+import {BASE_URL} from '../Utils/Utils';
+import {Navigate} from 'react-router-dom';
+import qs from 'qs';
 
 export const Login = () => {
 
+    const [username,setUsername] = useState('');
+    const [password,setPassword] = useState('');
+    const [navigate,setNavigate] = useState(false);
+
+    const send = async e => {
+        e.preventDefault();
+
+        const response = axios({
+            method: 'POST',
+            url: BASE_URL+"/api/login",
+            data: qs.stringify({
+              username: username,
+              password: password
+            }),
+            headers: {
+              'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+            }
+        });
+        
+        // const response = await axios.post(BASE_URL+"/api/login",{
+        //     username,password
+        // },{withCredentials:true});
+
+        console.log(response.data);
+
+        // setNavigate(true);
+    }
+
+    if(navigate){
+        return <Navigate to="/"/>
+    }
+
     return <main className="form-signin">
-    <form>
+    <form onSubmit={send}>
       <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
 
       <div className="form-floating">
-        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-        <label for="floatingInput">Email address</label>
+        <input type="text" className="form-control" id="floatingInput" placeholder="username"
+            onChange={e => setUsername(e.target.value)}/>
+        <label htmlFor="floatingInput">Username</label>
       </div>
       <div className="form-floating">
-        <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
-        <label for="floatingPassword">Password</label>
+        <input type="password" className="form-control" id="floatingPassword" placeholder="Password"
+            onChange={e => setPassword(e.target.value)}/>
+        <label htmlFor="floatingPassword">Password</label>
       </div>
 
       <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
