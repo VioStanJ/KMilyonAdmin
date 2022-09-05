fc3b4b8f174bfe8c3b78cb83b6668970d97f53d0
vsguerrier <vsguerrier@dinasa.ht>
2022-09-05 19:22:37 -0400

    add /

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import { Home } from './Pages/Home';
 import './Interceptors/axios';
 import Container from './Components/Container';
 import Ticket from './Pages/Ticket';
+import Welcome from './Pages/Welcome';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Welcome />} /> 
         <Route path="/login" element={<Login />} /> 
         <Route path="/home" element={<Container><Home /></Container>} /> 
         <Route path="/ticket" element={<Container><Ticket /></Container>} /> 
